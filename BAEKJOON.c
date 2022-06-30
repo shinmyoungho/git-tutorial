3b9750a1d7f4f5db2cb99219fcbda8f72c0768d4
shinmyoungho <lion1255@naver.com>
2022-06-30 20:25:31 +0900

    덮어 써버렸네

--- BAEKJOON.c
+++ BAEKJOON.c
@@ -2,29 +2,33 @@
 
 int main(void)
 {
-	int a[10], b[10];
-	int result = 0;
-	for (int i = 0; i < 10; i++)
+	int grade[20];
+	double new_grade[20];
+	int n;
+	double sum = 0.0;
+	
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
 	{
-		scanf_s("%d", &a[i]);               //배열안에 다른 숫자의 갯수를 구하려면 첫번째요소를 비교하면서
-		b[i] = a[i] % 42;				   //중복되는 값이 있으면 카운트를 하지않고 지나가면 된다.
-	}									   //이게 생각이 안나서 찾아보고 있네 ;; 이게 맞나
-
-	for (int i = 0; i < 10; i++)
+		scanf_s("%d", &grade[i]);
+	}
+	int max = grade[0];
+	for (int i = 1; i < n; i++)
 	{
-		int count = 0;
-		for (int j = i + 1; j < 10;j++)
+		if (max < grade[i])
 		{
-			if (b[i] == b[j])
-			{
-				count++;
-			}
-		}
-		if (count == 0)
-		{
-			result++;
+			max = grade[i];
 		}
 	}
-	printf("%d", result);
+	for (int i = 0; i < n; i++)
+	{
+		new_grade[i] = (double)grade[i] / max * 100;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		sum = sum + new_grade[i];
+	}
+	printf("%lf", sum / n);
 	return 0;
+
 }
